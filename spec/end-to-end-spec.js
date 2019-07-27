ce32a3b1e0c6678812b26fe8a7ffd5686e720f36
Peter Grainger <peter@grainger.xyz>
2019-07-27 21:03:53 +0100

    add redis to test setup

--- spec/end-to-end-spec.js
+++ spec/end-to-end-spec.js
@@ -1,7 +1,26 @@
 const request = require('supertest');
 const app = require('../src/app')
+const redis = require('redis')
 
 describe('/ip/check/:ipAddress', () => {
+
+  let client;
+
+  beforeAll(()=> {
+    client = redis.createClient();
+
+    client.on('connect', function() {
+        console.log('connected');
+    });
+
+    client.set('1.1.1.1', 'some_data for 1.1.1.1', function(err, reply) {
+      if(!err) console.log(reply)
+    })
+
+
+  })
+
+
   it('returns failure (403) when the ip is in the list', done => {
     request(app)
       .get('/ip/check/1.1.1.1')
